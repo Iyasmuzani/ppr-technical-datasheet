2324fe546c6b43a34125ed1dccc971d023d7e29b
ppr-technical-datasheet <iyasmuzani17@gmail.com>
2026-03-24 16:47:51 +0800

    refactor: consolidate profile address into a single line

--- js/utils/storage.js
+++ js/utils/storage.js
@@ -45,8 +45,8 @@ const Storage = {
   getProfile() {
     return this.get('profile') || {
       companyName: 'PT. Wahana Duta Jaya Rucika',
-      address: 'Alia Building, 7th Floor,',
-      'Jl.Ridwan Rais 10 – 18(Gambir) Jakarta 10110, Indonesia',
+      address1: 'Alia Building, 7th Floor',
+      address2: 'Jl.Ridwan Rais 10 – 18(Gambir) Jakarta 10110, Indonesia',
       phone: '(021) 386 7717',
       email: 'info@rucika.co.id',
       website: 'www.rucika.co.id'
